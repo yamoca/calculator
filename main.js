0fe4f34e489849e77b8b4890115402ab469892e0
leo <leojwgulliver@gmail.com>
2024-08-08 18:25:51 +0100

    implement naive method (only for multiply)

--- main.js
+++ main.js
@@ -24,9 +24,17 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-
+    return a * b; 
 }
 
 function divide(a, b) {
 
+}
+
+function calculate() {
+    if (textArea.textContent.includes("*")) {
+        let vals = textArea.textContent.split("*");
+        displayValue = multiply(vals[0], vals[1]);
+        textArea.textContent = displayValue; 
+    }
 }
